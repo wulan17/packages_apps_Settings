ramshell688/afterlife_packages_apps_Settings

Settings: Build AfterLab into Settings commit 122480e9795d751c48160e91ee51c54a884953e8 Author: Alex Cruz <alex@dirtyunicorns.com> Date:   Sat Jul 17 18:53:20 2021 +0000
    Settings: Add expandable bottombar for extensions

    Github Repo:- https://github.com/st235/ExpandableBottomBar

    Co-authored-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Change-Id: I310c396b474535ec1584ab164029755a4da18d84
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

commit b1a6e09bf2fd6d7933f6b38930b355df5062f8da
Author: maxwen <max.weninger@gmail.com>
Date:   Fri Sep 8 05:07:18 2017 +0530

    Build Extensions into Settings

      * Add it on dashboard
      * Set proper priority to show it below Apps & Notifications

    [jaysonedson: Ported to android11 and Android.bp]

    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: I594d89ad713a4735f8301c8e6372790812b2d86f
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

Change-Id: I433eafa37ab2810f4ff4fc2b06545bb93a04c527
Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -192,6 +192,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.afterlife.afterlab.CustomizeDashboard;
+
 public class SettingsGateway {
 
     /**
@@ -372,6 +374,7 @@ public class SettingsGateway {
             BatteryInfoFragment.class.getName(),
             UserAspectRatioDetails.class.getName(),
             ScreenTimeoutSettings.class.getName(),
+            CustomizeDashboard.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -418,5 +421,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
+            Settings.AftelabSettingsActivity.class.getName(),
     };
 }

wulan17/packages_apps_Settings

Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -192,6 +192,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.afterlife.afterlab.CustomizeDashboard;
+
 public class SettingsGateway {
 
     /**
@@ -372,6 +374,7 @@ public class SettingsGateway {
             BatteryInfoFragment.class.getName(),
             UserAspectRatioDetails.class.getName(),
             ScreenTimeoutSettings.class.getName(),
+            CustomizeDashboard.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -418,5 +421,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
+            Settings.AfterlabSettingsActivity.class.getName(),
     };
 }